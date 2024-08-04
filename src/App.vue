e34779b914e9ff09e38bd0ef47be464c33e752dc
poemsforaphrodite <pushpendersolanki_co21a6_23@dtu.ac.in>
2024-08-04 23:08:13 +0530

    initial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="iframe-container">
+    <iframe
+      src="https://ekkeltd-quiz.hf.space"
+      frameborder="0"
+      width="2000"
+      height="100%"
+    ></iframe>
+  </div>
+</template>
+
+<style scoped>
+.iframe-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
